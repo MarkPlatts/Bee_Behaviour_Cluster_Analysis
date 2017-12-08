adec00d7ad32a6700c756c37aecbdba6abf1061c
Mark Platts <markplatts23@gmail.com>
2017-12-08 17:35:59 +0000

    Fixed bug causing it to get stuck if there is a distance between points greater than the length of segment specified.

--- segment.py
+++ segment.py
@@ -70,17 +70,25 @@ class Segment:
         
     def getSegment(self, traj, lseg, ovlp, cum_dist_end_prev):
         
+        cumdist = traj['CumulativeDistance']
+        
         #find the beginning of this segment
         if cum_dist_end_prev != 0:
-            max_cum_dist_start_second = cum_dist_end_prev - lseg*ovlp
-            values_lesser = traj['CumulativeDistance'][traj['CumulativeDistance']<=max_cum_dist_start_second]
-            value_just_below = values_lesser.iloc[-1]
-            cumdist = traj['CumulativeDistance']
+
+            distance_to_previous_point = traj['Distance'][traj['CumulativeDistance'] == cum_dist_end_prev].index[0]
+            if distance_to_previous_point >= lseg:
+                value_just_below = cum_dist_end_prev    
+            else:
+                max_cum_dist_start_second = cum_dist_end_prev - lseg*ovlp
+                values_lesser = cumdist[cumdist <= max_cum_dist_start_second]
+                value_just_below = values_lesser.iloc[-1]
+                
             start_seg_index = cumdist[cumdist==value_just_below].index[0]
+            
         else:
             value_just_below = 0
             start_seg_index = 0
-        
+            
         #find the end of this segment
         values_greater = traj['CumulativeDistance'][traj['CumulativeDistance'] >= value_just_below + lseg]
         if values_greater.size == 0:
@@ -91,7 +99,8 @@ class Segment:
             last_value_segment = values_greater.iloc[0]
             cumdist = traj['CumulativeDistance']
             end_seg_index = cumdist[cumdist==last_value_segment].index[0]
-            segment = traj.loc[start_seg_index:end_seg_index,:].reset_index(drop=True)
+            segment = traj.loc[start_seg_index:end_seg_index,:]
+            segment = segment.reset_index(drop=True)
             end_of_trajectory = False
         
         return segment, last_value_segment, end_of_trajectory
